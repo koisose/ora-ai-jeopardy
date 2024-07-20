607469f43d8ec3832ee20580b4816b600bbe3716
koisose <plokotokbelajar@gmail.com>
2024-07-20 20:18:48 +0700

    push

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,21 +1,17 @@
 import "@rainbow-me/rainbowkit/styles.css";
-
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 import Ppa from "~~/components/0a/page";
 
 
 const ScaffoldEthApp = () => {
   return (
-    <html suppressHydrationWarning>
-      <body>
+    
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders><Ppa/></ScaffoldEthAppWithProviders>
         </ThemeProvider>
-      </body>
-    </html>
+    
   );
 };
 
